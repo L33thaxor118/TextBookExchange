2c000cf53de61dbc3bca0f237a1984c609648bab
Azriel <alvarado_118@yahoo.com>
2019-04-14 19:38:55 -0500

    added basic routing and login component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
+import UserAuthentication from './Sign_in/Sign_in'
 import logo from './logo.svg';
 import './App.css';
+import 'semantic-ui-css/semantic.css';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello world
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="pageContainer">
+          <Switch>
+            <Route exact path={process.env.PUBLIC_URL + '/'} component={UserAuthentication}/>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
