633de58177a62ca7c753ecb813018f4b697e134e
Srilakshmi Prasad <sprasad7@illinois.edu>
2019-04-14 19:29:21 -0500

    added improvements to App.js and Search.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
+/** 
+@author Srilakshmi Prasad
+**/
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
+// Include new Components here
+import Home from './Home/Home.js'
+import Search from './Search/Search.js';
+import Sign_in from './Sign_in/Sign_in.js';
+
+// Include any new stylesheets here
+// Note that components' stylesheets should NOT be included here.
+// They should be 'require'd in their component class file.
+require('./App.css');
+
 class App extends Component {
+  // Add to this as needed
+  constructor(props){
+		super(props);
+  	this.loginHandler = this.loginHandler.bind(this);
+	}
+	
+	// Meant to help pass user info to authorized components
+	loginHandler(userInfo){
+    this.setState({
+      userInfo:userInfo
+    })
+  }
+	
+	// Add components you want to test
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello world
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+			<Router>
+				<div>
+					<Route exact path="/" component={Search}  />
+    		</div>
+			</Router>
+      
     );
   }
 }
---
+++ src/Search/Search.js
@@ -0,0 +1,21 @@
+/** 
+@author Srilakshmi Prasad
+**/
+import React, { Component } from 'react'
+
+class Search extends Component {
+    constructor() {
+        super();
+        this.state = {
+        }
+    }
+
+    // add any functions & handlers here
+    render() {
+        return (
+            <div>Some JSX</div>
+        )
+    }
+}
+
+export default Search
