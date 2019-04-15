6676d71ac9739887577cca51762b91cbc95e7bdc
Alan Gan <alanrgan@gmail.com>
2019-04-15 13:27:57 -0500

    Fixed lint errors and remove log file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 // import 'semantic-ui-css/semantic.css';
 import './App.css';
 
 // Include new Components here
-import Home from './Home/Home'
-import Search from './Search/Search';
-import UserAuthentication from './Sign_in/Sign_in';
+// import Home from './Home/Home'
+import Search from './Search';
+import UserAuthentication from './Sign_in';
 
 // Currently displays Sign_in and Search components for testing purposes
 class App extends Component {
@@ -16,12 +16,12 @@ class App extends Component {
         <div className="pageContainer">
           <h3>Sign_in Component</h3>
           <Switch>
-            <Route exact path={process.env.PUBLIC_URL + '/'} component={UserAuthentication}/>
+            <Route exact path='/' component={UserAuthentication}/>
           </Switch>
         </div>
         <div>
           <h3>Search Component</h3>
-					<Route exact path="/" component={Search}/>
+					<Route exact path='/' component={Search}/>
     		</div>
       </Router>
     );
