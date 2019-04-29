e9faaadb0880ea843d4fd69cbd17d1eb674c480b
Alan Gan <alanrgan@gmail.com>
2019-04-29 12:49:46 -0500

    Refactor redux action creators

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,11 +1,10 @@
 import { combineReducers } from "redux";
 
-import {books, getBooksHasFailed, createBookHasFailed, listings, getListingsHasFailed,
+import {books, createBookHasFailed, listings, getListingsHasFailed,
   createListingHasFailed} from "./dataReducer";
 
 export default combineReducers({
   books,
-  getBooksHasFailed,
   createBookHasFailed,
 
   listings,
