94139cd0cea3282409ebb032de63804fe9a8fa83
Srilakshmi Prasad <sprasad7@illinois.edu>
2019-04-29 19:08:23 -0500

    file submission

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,31 +7,56 @@ import logo from './logo.png';
 import './Header.scss';
 
 class Header extends Component {
-  state = {
-    active_button: true
-  };
+  constructor(){
+    super();
 
-  changeColor() {
-    this.setState({
-      active_button: !this.state.active_button
-    });
+    this.state = {
+      btn_1_active: false,
+      btn_2_active: false,
+      btn_3_active: false
+    }
+  }
+
+  //Problem with color changing of buttons
+  changeColor(btn, event) {
+    if(btn=="b1"){
+      console.log("Changing state of b1");
+      this.setState({btn_1_active: true,  
+                     btn_2_active: false,
+                     btn_3_active: false});
+    }
+    if(btn=="b2"){
+      console.log("Changing state of b2");
+      this.setState({btn_1_active: false,  
+                     btn_2_active: true,
+                     btn_3_active: false});
+    }
+    if(btn=="b3"){
+      console.log("Changing state of b3");
+      this.setState({btn_1_active: false,  
+                     btn_2_active: false,
+                     btn_3_active: true});
+    }
   }
 
   render () {
-    let btn_class = this.state.active_button ? "inactive" : "active";
+    let btn_1 = this.state.btn_1_active ? "active" : "inactive";
+    let btn_2 = this.state.btn_2_active ? "active" : "inactive";
+    let btn_3 = this.state.btn_3_active ? "active" : "inactive";
+
     return (
       <div id="navbar">
-        <img id="logo" src={logo} alt="Logo" />
+        <a href="/" alt-text="Home"><img id="logo" src={logo} alt="Logo" /></a>
         <div id="navbar-right">
-          <a className={btn_class} onClick={this.changeColor.bind(this)} href="/">
-            Home
-          </a>
-          <a className={btn_class} onClick={this.changeColor.bind(this)} href="/listings">
-            Search Listings
-          </a>
-          <a className={btn_class} onClick={this.changeColor.bind(this)} href="/dashboard">
-            User Dashboard
-          </a>
+          <button className={btn_1} onClick={(e) => this.changeColor("b1", e)}>
+            <a href="/listings">Search Listings</a>
+          </button>
+          <button className={btn_2} onClick={(e) => this.changeColor("b2", e)}>
+            <a href="/listings/new">Create Listing</a>
+          </button>
+          <button className={btn_3} onClick={(e) => this.changeColor("b3", e)}>
+            <a href="/dashboard">User Dashboard</a>
+          </button>
         </div>
       </div>
     );
