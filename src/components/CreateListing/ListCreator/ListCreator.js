ae69956d1db0e4644531d79cbb7b9c06e72f5eae
Azriel <alvarado_118@yahoo.com>
2019-05-01 23:44:03 -0500

    fixed lint errors

--- src/components/CreateListing/ListCreator/ListCreator.js
+++ src/components/CreateListing/ListCreator/ListCreator.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Input, Button, Label} from 'semantic-ui-react';
+import {Input, Button} from 'semantic-ui-react';
 import {ListCreatorContainer} from './ListCreator.styled';
 
 class ListCreator extends Component {
