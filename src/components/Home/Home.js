3b9d6d5974b6b1771e2b4e7b338a74f57d8e18a8
Srilakshmi Prasad <sprasad7@illinois.edu>
2019-05-03 10:56:51 -0500

    FINISHED HOME

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,18 +2,26 @@
 @author Srilakshmi Prasad
 **/
 import React from 'react';
-import { Button } from 'semantic-ui-react';
 import listingsApi from '../../api/listings';
 import usersApi from '../../api/users';
 import { authentication, fetchPhotoUrls } from '../../utils/firebase';
 import './Home.scss';
 
-// Needs to be listing images from current user's wishlist. 
-// When you click on an image, it should open a listing (ideally)
 const imgUrls = [
+	"https://n1.sdlcdn.com/imgs/b/x/6/Computer-Systems-A-Programmer-s-SDL677179892-1-c5b32.jpg",
+	"https://www.booktopia.com.au/http_coversbooktopiacomau/600/9780813349800/0000/a-history-of-the-modern-middle-east.jpg",
+	"https://images.kogan.com/image/fetch/s--P-iJFyb7--/b_white,c_pad,f_auto,h_630,q_auto:good,w_1200/https://assets.kogan.com/images/booksbatch/BKT-9781118871645/html-css.jpg",
 	"https://pictures.abebooks.com/isbn/9780134082318-us.jpg",
-	"https://images-na.ssl-images-amazon.com/images/I/51m3Uj29JnL._SX372_BO1,204,203,200_.jpg"
+	"https://rukminim1.flixcart.com/image/704/704/book/0/4/7/physics-for-scientists-and-engineers-with-modern-physics-original-imaef2rkns7rqewk.jpeg?q=70"
 ];
+
+const searchQueries = [
+	"Computer Systems: A Programmer's Perspective",
+	"A History of the Modern Middle East",
+	"HTML and CSS: Design and Build Websites",
+	"Campbell Biology, 11th Ed",
+	"Physics for Scientists and Engineers with Modern Physics"
+]
 
 const Arrow = ({ direction, clickFunction, glyph }) => (
 	<div 
@@ -32,9 +40,7 @@ const ImageSlide = ({ url, onSlideClick, title }) => {
 	};
 	
 	return (
-		<div className="image-slide" style={styles}>
-			<Button onClick={() => onSlideClick(title)}></Button>
-		</div>
+		<div className="image-slide" style={styles} onClick={() => onSlideClick(title)}></div>
 	);
 }
 
@@ -117,10 +123,13 @@ class Home extends React.Component {
 	render () {
 		return (
             <div id="container">
-                <h1>Welcome! Browse Listings</h1>
+                <h1 className="heading">Welcome! Click on a Textbook to Search for Listings</h1>
                 <div className="carousel">
                     <Arrow direction="left" clickFunction={ this.previousSlide } glyph="&#9664;" />
-                    <ImageSlide url={ imgUrls[this.state.currentImageIndex] } onSlideClick={this.navigateToBookSearch.bind(this)} title={'A History of the Modern Middle East'} />
+					<ImageSlide 
+						url={ imgUrls[this.state.currentImageIndex] } 
+						onSlideClick={this.navigateToBookSearch.bind(this)} 
+						title={ searchQueries[this.state.currentImageIndex] } />
                     <Arrow direction="right" clickFunction={ this.nextSlide } glyph="&#9654;" />
                 </div>
             </div>
